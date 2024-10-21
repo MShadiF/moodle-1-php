d36783040284e30a49b7a4d6ef63ff1978b99e43
MShadiF <113097694+MShadiF@users.noreply.github.com>
2024-10-21 17:43:26 +0200

    Update lib_test.php

--- server/moodle/mod/livequiz/tests/lib_test.php
+++ server/moodle/mod/livequiz/tests/lib_test.php
@@ -28,7 +28,6 @@ final class lib_test extends \advanced_testcase {
     protected function setUp(): void {
         parent::setUp();
         $this->resetAfterTest(true);
-        require_once(_DIR_ . '/../lib.php');
     }
 
     /**
@@ -42,6 +41,7 @@ final class lib_test extends \advanced_testcase {
      * It should return false if the instance cannot be added.
      */
     public function test_livequiz_add_instance(): void {
+        require_once(__DIR__ . '/../lib.php');
         global $DB;
 
         $quizdata = new \stdClass(); // Create a new stdClass object (empty object).
